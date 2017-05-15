845fad949599bd37b72666521cba425d6dc58e3e
Mtaandao <mtaandao@gmail.com>
2017-05-15 11:30:03 +0300

    V2 Mail

--- mail.php
+++ mail.php
@@ -25,3 +25,44 @@ if(mail($to, $subject, $message, $headers)){
     echo 'Unable to send email. Please try again.';
 }
 ?>
+<?php
+$to = 'user@example.com';
+$subject = "Beautiful HTML Email using PHP by CodexWorld";
+
+$htmlContent = '
+    <html>
+    <head>
+        <title>Welcome to CodexWorld</title>
+    </head>
+    <body>
+        <h1>Thanks you for joining with us!</h1>
+        <table cellspacing="0" style="border: 2px dashed #FB4314; width: 300px; height: 200px;">
+            <tr>
+                <th>Name:</th><td>CodexWorld</td>
+            </tr>
+            <tr style="background-color: #e0e0e0;">
+                <th>Email:</th><td>contact@codexworld.com</td>
+            </tr>
+            <tr>
+                <th>Website:</th><td><a href="http://www.codexworld.com">www.codexworld.com</a></td>
+            </tr>
+        </table>
+    </body>
+    </html>';
+
+// Set content-type header for sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// Additional headers
+$headers .= 'From: CodexWorld<sender@example.com>' . "\r\n";
+$headers .= 'Cc: welcome@example.com' . "\r\n";
+$headers .= 'Bcc: welcome2@example.com' . "\r\n";
+
+// Send email
+if(mail($to,$subject,$htmlContent,$headers)):
+    $successMsg = 'Email has sent successfully.';
+else:
+    $errorMsg = 'Email sending fail.';
+endif;
+?>
